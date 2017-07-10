4acee48c62cc86a6afc827ca97061c901e521534
mgrinko <glukym@gmail.com>
2017-07-10 21:29:42 +0300

    template function was added

--- js/phone-catalogue.js
+++ js/phone-catalogue.js
@@ -5,22 +5,16 @@ class PhoneCatalogue {
     this._el = options.el;
     this._phones = options.phones;
 
+    const template = document.getElementById('phone-catalogue-template').innerHTML;
+
+    this._compiledTemplate = _.template(template);
+
     this._render();
   }
 
   _render() {
-    this._el.innerHTML = `
-      <ul class="phones">
-        <li class="thumbnail">
-          <a href="#!/phones/motorola-xoom-with-wi-fi" class="thumb">
-            <img alt="Motorola XOOM™ with Wi-Fi" src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-          </a>
-          <a href="#!/phones/motorola-xoom-with-wi-fi">Motorola XOOM™ with Wi-Fi</a>
-          <p>The Next, Next Generation
-
-            Experience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb).</p>
-        </li>
-      </ul>
-    `;
+    this._el.innerHTML = this._compiledTemplate({
+      phones: this._phones
+    });
   }
 }
